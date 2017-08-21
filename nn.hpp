f287264a08baa2593c41b84abab1f36ddb63d420
ttoocs <ttoocs@gmail.com>
2017-08-20 22:00:18 -0600

    Added simple test case to test increadibly simple forward propigation.... it failed.

---
+++ nn.hpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vexcl/vexcl.hpp>
+
+
+namespace nn{
+
+  //VEXCL simple dense multiplication.
+  template <class M, class V>
+  auto vexmult(size_t n, size_t m, M &&A, V &&x) {
+    using namespace vex;
+    auto NxM = extents[n][m];
+    return reduce<SUM>(NxM, A * reshape(x, NxM, extents[1]), 1);
+  }
+
+
+  //LAYER
+
+  template<typename ValType>
+  class layer{
+    public:
+      int type;
+      int width;
+      int height;
+      vex::vector<ValType> weights;
+
+      vex::vector<ValType> forward(vex::vector<ValType> input){
+        return vexmult<ValType,ValType> (height,width,input,weights);
+      }
+
+      vex::vector<ValType> backward(vex::vector<ValType> result) {
+        std::cout << "TODO" << std::endl;
+      }
+
+  };
+
+
+  //NETWORK
+
+  template<typename ValType>
+  class net{
+    public:
+      vex::vector<layer<ValType>> layers;
+
+      vex::vector<ValType> forward(vex::vector<ValType> input){
+        auto tmp = input;
+        for(int i=0; i<layers.size(); i++){
+          tmp = layers[i].forward(tmp);
+        }
+      }
+
+      vex::vector<ValType> backward(vex::vector<ValType> result){
+        std::cout << "TODO" << std::endl;
+      }
+  };
+
+}
