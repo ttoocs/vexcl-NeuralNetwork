66f20d397e133cd6043eae3186c2c272b2434786
ttoocs <ttoocs@gmail.com>
2017-09-10 14:10:07 -0600

    Got multiplication and random values working. Need to initalize the vector with it's size first.

--- main.cpp
+++ main.cpp
@@ -8,14 +8,36 @@ void testLayer(){
   nn::layer<double> testLayer;
   testLayer.width=32;
   testLayer.height=32;
+  testLayer.weights.resize(32*32);
   vex::Random<double, vex::random::threefry> rnd;
-  testLayer.weights = 4 * rnd(vex::element_index(), std::rand()) - 1;
+  testLayer.weights = 2 * rnd(vex::element_index(0,32*32), std::rand()) -1;
 
   vex::vector<double> input(32);
+  input = 2 * rnd(vex::element_index(), std::rand()) -1 ;
+//  we = rnd(vex::element_index(0,10), std::rand());
+//
+//  std::vector<double> test(10);
+//  test.push_back(32);
+
+//  vex::copy(we,test);
+
+//  std::cout << testLayer.weights << std::endl;
+
+//  std::cout << input << std::endl;
+
+// for (std::vector<double>::const_iterator i = test.begin(); i != test.end(); ++i)
+//    std::cout << *i << ',';
+//  std::cout << std::endl;
+
+//  std::cout << test << std::endl;
+
+//  vex::vector<double> input(32*32);
+//  vex::vector<double> input = 4 * rnd(vex::element_index(0,32*32), std::rand()) - 1;
+  
 
   auto ret = testLayer.forward(input);
 
-  std::cout << ret  << std::endl;
+  std::cout << input  << std::endl;
 }
 
 
--- nn.hpp
+++ nn.hpp
@@ -5,13 +5,15 @@
 namespace nn{
 
   //VEXCL simple dense multiplication.
+/*
   template <class M, class V>
-  auto vexmult(size_t n, size_t m, M &&A, V &&x) {
+//  auto vexmult(size_t n, size_t m, M &&A, V &&x) {
+  auto vexmult(size_t n, size_t m, M A, V x) {
     using namespace vex;
     auto NxM = extents[n][m];
     return reduce<SUM>(NxM, A * reshape(x, NxM, extents[1]), 1);
   }
-
+ */
 
   //LAYER
 
@@ -24,7 +26,9 @@ namespace nn{
       vex::vector<ValType> weights;
 
       vex::vector<ValType> forward(vex::vector<ValType> input){
-        return vexmult<ValType,ValType> (height,width,input,weights);
+//        auto NxM = vex::extents[height][width];
+//        return vex::reduce<vex::SUM>(NxM, input * vex::reshape(weights, NxM, vex::extents[1]), 1);
+        return vexmult<vex::vector<ValType>,vex::vector<ValType>> (height,width,input,weights);
       }
 
       vex::vector<ValType> backward(vex::vector<ValType> result) {
